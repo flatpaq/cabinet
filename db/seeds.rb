09cb5d1bec9095570e84d404c069b94fd1badba5
flatpaq <flatpaq.net@gmail.com>
2021-09-06 11:51:21 +0900

    seedファイルを追加

---
+++ db/seeds.rb
@@ -0,0 +1,47 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |n|
+  User.create!(
+    name_id: "admin#{n+1}",
+    name: "アドミンユーザ#{n + 1}",
+    email: "admin#{n + 1}@sample.com",
+    password: "password",
+    password_confirmation: "password",
+    introduction: "テスト用ユーザ#{n + 1}です",
+    admin: true,
+    state: true,
+    indication: false
+  )
+
+end
+
+10.times do |n|
+  Tag.create!(
+    slug: "tag#{n+1}",
+    label: "タグその#{n + 1}",
+    edit_permit: 0
+  )
+end
+
+
+User.all.each_with_index do |user, n|
+  user.articles.create!(
+    user_id: user.id,
+    title: "タイトル#{n+1}",
+    content: "テキスト#{n + 1}",
+    status: 1,
+    coedit_permit: 0,
+    garbage: false
+  )
+end
+
+TagAssignment.create!(
+  article_id: 1, 
+  tag_id: 1
+)
