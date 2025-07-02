bdc8ed9b912b228bf9ecd5eae9d9e7c32a9ff8e1
FLATPAQ <flatpaq.net@gmail.com>
2025-07-02 19:09:30 +0900

    for render

--- babel.config.js
+++ babel.config.js
@@ -66,6 +66,24 @@ module.exports = function(api) {
         {
           async: false
         }
+      ],
+      [
+        "@babel/plugin-transform-class-properties", 
+        { 
+          "loose": true   
+        }
+      ],
+      [
+        "@babel/plugin-transform-private-methods", 
+        { 
+          "loose": true   
+        }
+      ],
+      [
+        "@babel/plugin-transform-private-property-in-object", 
+        { 
+          "loose": true   
+        }
       ]
     ].filter(Boolean)
   }
