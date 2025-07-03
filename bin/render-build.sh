04314a0ac7b8046c8b950a3df60c4d7a1c12bd88
FLATPAQ <flatpaq.net@gmail.com>
2025-07-03 23:38:09 +0900

    fixed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -11,8 +11,5 @@ bundle exec rails assets:clean
 # Uncomment the following line:
 bundle exec rails db:migrate
 
-# bundle exec rails db:drop
-# bundle exec rails db:create
-
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
-bundle exec rails db:seed
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+# bundle exec rails db:seed
