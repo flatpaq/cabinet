db179ae9085a3eede727672fa00d990148c29fed
FLATPAQ <flatpaq.net@gmail.com>
2025-07-03 15:06:03 +0900

    fixed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,9 +9,10 @@ bundle exec rails assets:clean
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
+# bundle exec rails db:migrate
 
 # bundle exec rails db:drop
 # bundle exec rails db:create
 
-bundle exec rails db:migrate
-# bundle exec rails db:seed
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+bundle exec rails db:seed
