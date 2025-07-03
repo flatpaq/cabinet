21a518ff7103ec6702ed5d46134628dc53493382
FLATPAQ <flatpaq.net@gmail.com>
2025-07-03 15:27:18 +0900

    fixed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -14,5 +14,5 @@ bundle exec rails assets:clean
 # bundle exec rails db:drop
 # bundle exec rails db:create
 
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
-bundle exec rails db:seed
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+# bundle exec rails db:seed
